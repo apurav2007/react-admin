d3eb77ccfb84ead0e1d703a8b8853322fe05130d
apurav2007 <apurav2007@gmail.com>
2017-04-26 00:08:24 +0530

    Add Forgot Password Page

---
+++ src/views/Pages/Forgot/Forgot.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Forgot extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className="card-group mb-0">
+              <div className="card p-2">
+                <div className="card-block">
+                  <h1>Forgot Password</h1>
+                  <p className="text-muted">Email your email for reset password link.</p>
+                  <div className="input-group mb-1">
+                    <span className="input-group-addon"><i className="icon-envelope-open"></i></span>
+                    <input type="text" className="form-control" placeholder="Email"/>
+                  </div>
+                  
+                  <div className="row">
+                    <div className="col-6">
+                      <button type="button" className="btn btn-primary px-2">Reset Password</button>
+                    </div>
+                    <div className="col-6 text-right">
+                      <button type="button" className="btn btn-link px-0">Back</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Forgot;
