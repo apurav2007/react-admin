534f5e5037a3312021127c93ca97704234564ab4
apurav2007 <apurav2007@gmail.com>
2017-05-02 23:48:24 +0530

    Add common component for input

---
+++ src/views/Components/Common/TextFieldGroupIcon.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import classnames from 'classnames';
+
+const TextFieldGroup = ({ field, value, label, error, type, onChange, checkUserExists,icon }) => {
+  return (
+    <div className={classnames('input-group mb-1', { 'has-error': error })}>
+      <span className="input-group-addon"><i className={icon}></i></span>
+      <input
+        onChange={onChange}
+        onBlur={checkUserExists}
+        value={value}
+        type={type}
+        placeholder={label}
+        name={field}
+        className="form-control"
+      />
+    {error && <span style={{width: 100 + '%'}} className="help-block">{error}</span>}
+    </div>  );
+}
+
+TextFieldGroup.propTypes = {
+  field: React.PropTypes.string.isRequired,
+  value: React.PropTypes.string.isRequired,
+  label: React.PropTypes.string.isRequired,
+  error: React.PropTypes.string,
+  type: React.PropTypes.string.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  checkUserExists: React.PropTypes.func
+}
+
+TextFieldGroup.defaultProps = {
+  type: 'text'
+}
+
+export default TextFieldGroup;
--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import TextFieldGroup from '../../Components/Common/TextFieldGroup';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import TextFieldGroup from '../../Components/Common/TextFieldGroupIcon';
+import validateInput from '../../../validation/login';
 
 class Login extends Component {
   constructor(props) {
@@ -16,16 +17,26 @@ class Login extends Component {
     this.onChange = this.onChange.bind(this);
   }
 
+  isValid() {
+   const { errors, isValid } = validateInput(this.state);
+   console.log(errors);
+   if (!isValid) {
+     this.setState({ errors });
+   }
+
+   return isValid;
+ }
+
   onSubmit(e) {
     e.preventDefault();
-    if (this.isValid()) {
+  
+    if (this.isValid()) {   alert('111');
       this.setState({ errors: {}, isLoading: true });
       this.props.login(this.state).then(
         (res) => this.context.router.push('/'),
         (err) => this.setState({ errors: err.response.data.errors, isLoading: false })
       );
-    }
-  }
+    }}
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
@@ -40,31 +51,35 @@ class Login extends Component {
           <div className="col-md-4">
             <div className="card-group mb-0">
               <div className="card p-2">
+                <form onSubmit={this.onSubmit}>
                 <div className="card-block">
                   <h1>Login</h1>
+                  { errors.form && <div className="alert alert-danger">{errors.form}</div> }
                   <p className="text-muted">Sign In to your account</p>
-                  <div className="input-group mb-1">
-                    <span className="input-group-addon"><i className="icon-envelope-open"></i></span>
-                    <TextFieldGroup
-                      field="email"
-                      value={email}
-                      error={errors.email}
-                      onChange={this.onChange}
-                    />
-                  </div>
+
+                  <TextFieldGroup
+                    field="email"
+                    label="Email"
+                    value={email}
+                    error={errors.email}
+                    onChange={this.onChange}
+                    icon="icon-envelope-open"
+                  />
                   <div className="input-group mb-2">
                     <span className="input-group-addon"><i className="icon-lock"></i></span>
-                    <input type="password" className="form-control" error={errors.password}  onChange={this.onChange} placeholder="Password"/>
+                    <input type="password" className="form-control" name={password} onChange={this.onChange} placeholder="Password"/>
                   </div>
+
                   <div className="row">
                     <div className="col-6">
-                      <button type="submit" className="btn btn-primary px-2">Login</button>
+                      <button  disabled={isLoading} className="btn btn-primary px-2">Login</button>
                     </div>
                     <div className="col-6 text-right">
                       <Link to={'/pages/forgot'} className="btn btn-link px-0" > Forgot password?</Link>
                     </div>
                   </div>
                 </div>
+                </form>
               </div>
             </div>
           </div>
