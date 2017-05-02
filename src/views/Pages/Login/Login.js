323c06372036c2d86e9e4e9d6aba707ebf039728
apurav2007 <apurav2007@gmail.com>
2017-05-02 18:21:10 +0530

    Update login and forgot page

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
+import TextFieldGroup from '../../Components/Common/TextFieldGroup';
+import { Link } from 'react-router'
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errors: {},
+      isLoading: false
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    if (this.isValid()) {
+      this.setState({ errors: {}, isLoading: true });
+      this.props.login(this.state).then(
+        (res) => this.context.router.push('/'),
+        (err) => this.setState({ errors: err.response.data.errors, isLoading: false })
+      );
+    }
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+
   render() {
+    const { errors, email, password, isLoading } = this.state;
     return (
       <div className="container">
         <div className="row justify-content-center">
@@ -13,18 +45,23 @@ class Login extends Component {
                   <p className="text-muted">Sign In to your account</p>
                   <div className="input-group mb-1">
                     <span className="input-group-addon"><i className="icon-envelope-open"></i></span>
-                    <input type="text" className="form-control" placeholder="Email"/>
+                    <TextFieldGroup
+                      field="email"
+                      value={email}
+                      error={errors.email}
+                      onChange={this.onChange}
+                    />
                   </div>
                   <div className="input-group mb-2">
                     <span className="input-group-addon"><i className="icon-lock"></i></span>
-                    <input type="password" className="form-control" placeholder="Password"/>
+                    <input type="password" className="form-control" error={errors.password}  onChange={this.onChange} placeholder="Password"/>
                   </div>
                   <div className="row">
                     <div className="col-6">
-                      <button type="button" className="btn btn-primary px-2">Login</button>
+                      <button type="submit" className="btn btn-primary px-2">Login</button>
                     </div>
                     <div className="col-6 text-right">
-                      <button type="button" className="btn btn-link px-0">Forgot password?</button>
+                      <Link to={'/pages/forgot'} className="btn btn-link px-0" > Forgot password?</Link>
                     </div>
                   </div>
                 </div>
